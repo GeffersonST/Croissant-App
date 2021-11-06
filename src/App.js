0c0fa4e6479c544c0fdfaaf799558a15cc505036
GeffersonST <geffsevero@gmail.com>
2021-11-06 18:53:56 -0300

    Imagens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero/index";
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
+      <Hero />
     </Router>
   );
 }
