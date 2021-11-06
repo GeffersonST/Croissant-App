718c04a4b80abc299df1061acdcfe964767cbcee
GeffersonST <geffsevero@gmail.com>
2021-11-06 18:18:05 -0300

    Croissant Icon

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
+import { GiCroissant } from "react-icons/gi";
 
 export const Nav = styled.nav`
   background: red;
@@ -20,4 +21,21 @@ export const NavLink = styled(Link)`
     top: 10px;
     left: 25px;
   }
+`;
+export const NavIcon = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  color: #fff;
+
+  p {
+    transform: translate(-175%, 100%);
+    font-weight: bold;
+  }
+`;
+export const Bars = styled(GiCroissant)`
+  font-size: 2rem;
+  transform: translate(-50%, -15%);
 `;
