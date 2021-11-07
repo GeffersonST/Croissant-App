da3126d193cc6b04693464803a0869741d55073a
GeffersonST <geffsevero@gmail.com>
2021-11-07 20:24:54 -0300

    Inserindo Imagem dos Produtos

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,30 +1,30 @@
 import product1 from "../../images/croissant-8.jpg";
-import product2 from "../../images/croissant-6.jpg";
-import product3 from "../../images/croissant-9.jpg";
+import product2 from "../../images/croissaint-bacon1.jpg";
+import product3 from "../../images/croissant-doce1.jpg";
 
 export const productData = [
   {
     img: product1,
-    alt: "Pizza",
+    alt: "Croissant",
     name: "Croissant Salgado",
     desc: "Tomate, Orégano, Azeite de Oliva",
-    price: "R$:2,50",
+    price: "R$: 5.00",
     button: "Add no Carrinho",
   },
   {
     img: product2,
-    alt: "Pizza",
-    name: "Croissant Salgado",
-    desc: "Tomate, Orégano, Azeite de Oliva",
-    price: "R$:2,50",
+    alt: "Croissant",
+    name: "Croissant Bacon",
+    desc: "Bacon, Alface, Queijo",
+    price: "R$: 10.00",
     button: "Add no Carrinho",
   },
   {
     img: product3,
-    alt: "Pizza",
-    name: "Croissant Salgado",
-    desc: "Tomate, Orégano, Azeite de Oliva",
-    price: "R$:2,50",
+    alt: "Croissant",
+    name: "Croissant Doce",
+    desc: "Doce de Leite, Morango, Geléia",
+    price: "R$: 10.00",
     button: "Add no Carrinho",
   },
 ];
